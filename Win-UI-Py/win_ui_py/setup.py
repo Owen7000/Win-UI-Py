9e5da7dd1a07c5e4f4172d69677de15f5050d4b7
Owen <gw10plimerowen2@glow.sch.uk>
2022-11-15 10:16:29 +0000

    Initial commit and small changes

---
+++ Win-UI-Py/win_ui_py/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from __init__ import version as ver
+
+setup(
+  name = "Win UI Py",
+  version = ver,
+  description = "A UI module for python that doesn't look like it was created in the early 90's",
+  author = "Owen P",
+  author_email = "Owen7000@programmer.net",
+  url = "https://github.com/Owen7000/Win-UI-Py"
+)
